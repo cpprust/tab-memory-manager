c72955634d6035696204e2af902a4eb5b269254d
cpprust <cpprust@gmail.com>
2024-12-08 12:54:25 +0800

    Move update status logic to status module

--- src/status.rs
+++ src/status.rs
@@ -5,6 +5,7 @@ use sysinfo::{Pid, System};
 use crate::{
     tab_data_requester::{TabInfo, Timestamp},
     tab_killer::Rss,
+    BROWSER_NAME,
 };
 
 /// App status that must be sharing between threads
@@ -18,6 +19,64 @@ pub struct Status {
 }
 
 impl Status {
+    pub fn update(&mut self, new_tab_infos: HashMap<Pid, TabInfo>, timestamp: Timestamp) {
+        self.tab_infos = new_tab_infos;
+        self.timestamp = timestamp;
+        // Clear stat if browser closed
+        let browser_process_count = self
+            .system
+            .processes_by_exact_name(BROWSER_NAME.as_ref())
+            .count();
+        if browser_process_count == 0 {
+            self.tab_infos.clear();
+        }
+
+        // Update begin_background_timestamps
+        let data_timestamp = self.timestamp;
+        let mut last_access_timestamps: HashMap<Pid, Timestamp> = self
+            .tab_infos
+            .iter()
+            .filter(|(_, tab_info)| tab_info.title != "New Tab")
+            .map(|(&pid, tab_info)| {
+                if tab_info.active {
+                    (pid, data_timestamp)
+                } else {
+                    (pid, tab_info.last_accessed)
+                }
+            })
+            .collect();
+        last_access_timestamps
+            .iter_mut()
+            .for_each(|(pid, last_accessd_time)| {
+                if let Some(&begin_background_timestamp) = self.begin_background_timestamps.get(pid)
+                {
+                    *last_accessd_time = last_accessd_time.max(begin_background_timestamp);
+                }
+            });
+        self.begin_background_timestamps = last_access_timestamps;
+
+        // Update begin_cpu_idle_timestamps
+        let new_begin_cpu_idle_timestamps: HashMap<Pid, Timestamp> = self
+            .tab_infos
+            .keys()
+            .filter_map(|&pid| match self.begin_cpu_idle_timestamps.get(&pid) {
+                Some(&old_begin_cpu_idle_timestamp) => {
+                    if let Some(process) = self.system.processes().get(&pid) {
+                        if process.cpu_usage() == 0.0 {
+                            Some((pid, old_begin_cpu_idle_timestamp))
+                        } else {
+                            Some((pid, self.timestamp))
+                        }
+                    } else {
+                        None
+                    }
+                }
+                None => Some((pid, self.timestamp)),
+            })
+            .collect();
+        self.begin_cpu_idle_timestamps = new_begin_cpu_idle_timestamps;
+    }
+
     pub fn get_sorted_pid_rss(&self) -> Vec<(Pid, Rss)> {
         // The background tab processes pid and rss, sorted by rss
         let mut sorted_pid_rss: Vec<(Pid, u64)> = self
